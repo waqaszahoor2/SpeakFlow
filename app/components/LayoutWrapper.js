65fbf40ad76e9b2c2632dc03fafb9707cdb8c404
waqaszahoor2 <akashz76961@gmail.com>
2026-05-14 10:47:10 +0500

    add

--- app/components/LayoutWrapper.js
+++ app/components/LayoutWrapper.js
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import Sidebar from './Sidebar';
 import { SidebarProvider, useSidebar } from '../context/SidebarContext';
